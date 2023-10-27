830d399eac76a24fd7820230b3d340e5cdbf3407
Sina Rafiei <msina.raf@gmail.com>
2023-10-27 09:20:15 -0400

    put production to docker-compose

--- start_local.sh
+++ start_local.sh
@@ -2,7 +2,7 @@
 
 
 # Build the Docker images without using cache
-docker-compose --env-file .env.development build --no-cache
+docker-compose -f docker-compose.development.yml  --env-file .env.development build --no-cache
 
 # Then start the services
 docker-compose --env-file .env.development up -d
--- start_production.sh
+++ start_production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build the Docker images without using cache for the production setup
-docker-compose -f docker-compose.production.yml --env-file .env.production build --no-cache
+docker-compose --env-file .env.production build --no-cache
 
 docker push registry.digitalocean.com/farabix/mainframe:latest
