14ac853981a77b8b6b3d50cfbc60dc5d01d29e11
ario rafiei <ario.rafiei.1997@gmail.com>
2023-12-14 10:35:19 -0500

    added keyword and categories

--- app/controllers/commonController.py
+++ app/controllers/commonController.py
@@ -1,50 +1,65 @@
 # app/controllers/auth_controller.py
 from fastapi import APIRouter, Depends, Request, UploadFile, File, Form
 from typing import Annotated
-from app.config.dependencies import get_db,oauth2_bearer, db_dependency
+from app.config.dependencies import db_dependency
 from app.services.authService import get_current_user
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from app.util.fileUpload import upload_to_spaces
-from app.services.commonService import save_media
-from fastapi import UploadFile, HTTPException
-from secrets import token_hex
-import magic  # Ensure python-magic is installed
+from app.services.commonService import add_keyword, update_keyword, delete_keyword, upload_media, add_category, update_category, delete_category, get_keyword, get_sub_category, get_category
 
 
 router = APIRouter(prefix="/common", tags=["common"])
 limiter = Limiter(key_func=get_remote_address)
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
-ALLOWED_MIME_TYPES = ["application/pdf", "image/jpeg", "image/png", "image/jpg"]  # Add allowed MIME types
-MAX_FILE_SIZE = 10 * 1024 * 1024  # 10 MB
 
 @router.post("/upload/")
 @limiter.limit("10/minute")
 async def upload_document(request: Request, user: user_dependency, db: db_dependency, file_type: str = Form(...),file: UploadFile = File(...)):
-    # Read the contents of the file
-    contents = await file.read()
+    return upload_media(db, file_type, file)
 
-    # Check the file size
-    if len(contents) > MAX_FILE_SIZE:
-        raise HTTPException(status_code=413, detail="File size exceeds the allowed limit.")
+@router.get("/keyword")
+@limiter.limit("10/minute")
+async def get_keyword(request: Request, user: user_dependency, db: db_dependency, keyword_id: int):
+    return get_keyword(db, keyword_id)
 
-    # Get MIME type of the file
-    mime_type = magic.from_buffer(contents, mime=True)
-    if mime_type not in ALLOWED_MIME_TYPES:
-        raise HTTPException(status_code=415, detail="Unsupported file type.")
+@router.post("/keyword")
+@limiter.limit("10/minute")
+async def add_keyword(request: Request, user: user_dependency, db: db_dependency, keyword: str):
+    return add_keyword(db, keyword)
 
-    # Reset file to the beginning after reading
-    file.file.seek(0)
+@router.put("/keyword")
+@limiter.limit("10/minute")
+async def update_keyword(request: Request, user: user_dependency, db: db_dependency, keyword: str, new_keyword: str):
+    return update_keyword(db, keyword, new_keyword)
 
-    # Upload the file to DigitalOcean Spaces
-    file_ext = file.filename.split(".")[-1]
-    file_name = token_hex(16)
-    file_path = file_type + "/"
-    url = await upload_to_spaces(file_name, file_path, file_ext, file)
+@router.delete("/keyword")
+@limiter.limit("10/minute")
+async def delete_keyword(request: Request, user: user_dependency, db: db_dependency, keyword: str):
+    return delete_keyword(db, keyword)
 
-    # Save the URL in the database
-    #save_media(db: Session, file_name: str, file_type: str, file_extension: str)
-    media_record = await save_media(db, file_name, file_type, file_ext)
 
-    return {"file_name": file_name, "id": media_record.id}
+@router.get("/category/individual")
+@limiter.limit("10/minute")
+async def get_category(request: Request, user: user_dependency, db: db_dependency, category: str):
+    return get_category(db, category)
+
+@router.get("/category/children")
+@limiter.limit("10/minute")
+async def get_sub_category(request: Request, user: user_dependency, db: db_dependency, parent_id: int):
+    return get_sub_category(db, parent_id)
+
+@router.post("/category")
+@limiter.limit("10/minute")
+async def add_category(request: Request, user: user_dependency, db: db_dependency, category: str):
+    return add_category(db, category)
+
+@router.put("/category")
+@limiter.limit("10/minute")
+async def update_category(request: Request, user: user_dependency, db: db_dependency, category: str, new_category: str):
+    return update_category(db, category, new_category)
+
+@router.delete("/category")
+@limiter.limit("10/minute")
+async def delete_category(request: Request, user: user_dependency, db: db_dependency, category: str):
+    return delete_category(db, category)
--- app/services/commonService.py
+++ app/services/commonService.py
@@ -1,5 +1,43 @@
 from sqlalchemy.orm import Session
 from app.models.common import Media
+# app/controllers/auth_controller.py
+from fastapi import UploadFile
+from app.util.fileUpload import upload_to_spaces
+from app.services.commonService import save_media
+from fastapi import UploadFile, HTTPException
+from secrets import token_hex
+from app.models.common import Media, Keyword, Category
+import magic  # Ensure python-magic is installed
+
+ALLOWED_MIME_TYPES = ["application/pdf", "image/jpeg", "image/png", "image/jpg"]  # Add allowed MIME types
+MAX_FILE_SIZE = 10 * 1024 * 1024  # 10 MB
+
+async def upload_media(db: Session, file_type: str, file: UploadFile):
+    # Read the contents of the file
+    contents = await file.read()
+
+    # Check the file size
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=413, detail="File size exceeds the allowed limit.")
+
+    # Get MIME type of the file
+    mime_type = magic.from_buffer(contents, mime=True)
+    if mime_type not in ALLOWED_MIME_TYPES:
+        raise HTTPException(status_code=415, detail="Unsupported file type.")
+
+    # Reset file to the beginning after reading
+    file.file.seek(0)
+
+    # Upload the file to DigitalOcean Spaces
+    file_ext = file.filename.split(".")[-1]
+    file_name = token_hex(16)
+    file_path = file_type + "/"
+    url = await upload_to_spaces(file_name, file_path, file_ext, file)
+
+    # Save the URL in the database
+    media_record = await save_media(db, file_name, file_type, file_ext)
+
+    return {"file_name": file_name, "id": media_record.id}
 
 
 async def save_media(db: Session, file_name: str, file_type: str, file_extension: str):
@@ -7,3 +45,62 @@ async def save_media(db: Session, file_name: str, file_type: str, file_extension
     db.add(new_media)
     db.commit()
     return new_media
+
+
+async def get_keyword(db: Session, keyword_id: int):
+    keyword = db.query(Keyword).filter(Keyword.id == keyword_id).first()
+    return keyword
+
+async def add_keyword(db: Session, keyword: str):
+    new_keyword = Keyword(name=keyword)
+    db.add(new_keyword)
+    db.commit()
+    return {"message": "Keyword added successfully"}
+
+async def update_keyword(db: Session, keyword: str, new_keyword: str):
+    existing_keyword = db.query(Keyword).filter(Keyword.name == keyword).first()
+    if existing_keyword is None:
+        return {"message": "Keyword not found"}
+    existing_keyword.name = new_keyword
+    db.commit()
+    return {"message": "Keyword updated successfully"}
+
+async def delete_keyword(db: Session, keyword: str):
+    existing_keyword = db.query(Keyword).filter(Keyword.name == keyword).first()
+    if existing_keyword is None:
+        return {"message": "Keyword not found"}
+    db.delete(existing_keyword)
+    db.commit()
+    return {"message": "Keyword deleted successfully"}
+
+async def add_category(db: Session, category: str):
+    new_category = Category(name=category)
+    db.add(new_category)
+    db.commit()
+    return {"message": "Category added successfully"}
+
+async def update_category(db: Session, category: str, new_category: str):
+    existing_category = db.query(Category).filter(Category.name == category).first()
+    if existing_category is None:
+        return {"message": "Category not found"}
+    existing_category.name = new_category
+    db.commit()
+    return {"message": "Category updated successfully"}
+
+async def delete_category(db: Session, category: str):
+    existing_category = db.query(Category).filter(Category.name == category).first()
+    if existing_category is None:
+        return {"message": "Category not found"}
+    db.delete(existing_category)
+    db.commit()
+    return {"message": "Category deleted successfully"}
+
+async def get_category(db: Session, category: str):
+    existing_category = db.query(Category).filter(Category.name == category).first()
+    if existing_category is None:
+        return {"message": "Category not found"}
+    return {"message": "Got category successfully", "category": existing_category.name}
+
+async def get_sub_category(db: Session, parent_id: int):
+    sub_categories = db.query(Category).filter(Category.parent_id == parent_id).all()
+    return sub_categories
