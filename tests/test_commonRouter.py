84149d2cc1559af02b14d258896131a50054cbf2
ario rafiei <ario.rafiei.1997@gmail.com>
2023-12-21 08:37:00 -0500

    changed the file upload unit tests

--- tests/test_commonRouter.py
+++ tests/test_commonRouter.py
@@ -7,6 +7,7 @@ sys.path.append(os.getcwd())
 from fastapi.testclient import TestClient
 from main import app
 from tests.test_authRouter import test_login_valid_user
+import requests
 
 client = TestClient(app)
 
@@ -15,24 +16,27 @@ def test_upload_file():
     jwt_token = test_login_valid_user()
 
     # Arrange for file upload
-    test_file_path = "./test_file.jpg"
+    test_file_url = "https://farabix-resources.nyc3.cdn.digitaloceanspaces.com/unittests/test_file.jpg"
 
     # Headers with Authorization
     headers = {"Authorization": f"Bearer {jwt_token}"}
 
-    # Act - Upload the file
-    with open(test_file_path, "rb") as file:
-        response = client.post("/common/file/upload", 
-                               headers=headers,
-                               files={"file": (test_file_path, file, "image/jpg")},
-                               data={"file_type": "website"})
+    # Act - Get the file content from the URL and upload the file
+    response = requests.get(test_file_url)
+    file_content = response.content
+    file_name = test_file_url.split('/')[-1]
+
+    upload_response = client.post("/common/file/upload",
+                                  headers=headers,
+                                  files={"file": (file_name, file_content, "image/jpg")},
+                                  data={"file_type": "website"})
 
     # Assert - Check upload success
-    assert response.status_code == 200
-    assert "file_name" in response.json()
+    assert upload_response.status_code == 200
+    assert "file_name" in upload_response.json()
 
     # Get the uploaded file name for deletion
-    uploaded_file_name = response.json()["file_name"]
+    uploaded_file_name = upload_response.json()["file_name"]
 
     return uploaded_file_name, jwt_token
 
@@ -40,22 +44,25 @@ def test_upload_invalid_file_type():
     # Login and get JWT token
     jwt_token = test_login_valid_user()
 
-
     # Arrange for file uploads
-    test_file_path = "./text_file.txt"
+    test_file_url = "https://farabix-resources.nyc3.cdn.digitaloceanspaces.com/unittests/text_file.txt"
+
     # Headers with Authorization
     headers = {"Authorization": f"Bearer {jwt_token}"}
 
-    # Act - Upload the file
-    with open(test_file_path, "rb") as file:
-        response = client.post("/common/file/upload", 
-                               headers=headers,
-                               files={"file": (test_file_path, file, "image/jpg")},
-                               data={"file_type": "website"})
+    # Act - Get the file content from the URL and upload the file
+    response = requests.get(test_file_url)
+    file_content = response.content
+    file_name = test_file_url.split('/')[-1]
 
-    # Assert - Check upload success
-    assert response.status_code == 415
-    assert response.json() == {"detail": "Unsupported file type."}
+    upload_response = client.post("/common/file/upload",
+                                  headers=headers,
+                                  files={"file": (file_name, file_content, "image/jpg")},
+                                  data={"file_type": "website"})
+
+    # Assert - Check upload failure
+    assert upload_response.status_code == 415
+    assert upload_response.json() == {"detail": "Unsupported file type."}
 
 def test_delete_uploaded_file():
     # Execute the upload test to get the necessary details
